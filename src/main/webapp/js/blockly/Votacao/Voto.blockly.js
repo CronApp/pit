05462fedf54762dba91e4c00a2fce8c6485ac961
José Zay <jose.zay@techne.com.br>
2023-08-17 17:58:43 +0000

    comentar blocos de código.

--- src/main/webapp/js/blockly/Votacao/Voto.blockly.js
+++ src/main/webapp/js/blockly/Votacao/Voto.blockly.js
@@ -7,17 +7,17 @@ window.blockly.js.blockly.Votacao.Voto = window.blockly.js.blockly.Votacao.Voto 
 /**
  * @function IniciarVotacao
  *
- *
+ * Oculta tela de aceite de regras, e exibe tela de votações efetuadas (vazia).
  *
  * @param checkAceitarRegras
  *
  * @author José Zay
- * @since 17/08/2023, 14:50:55
+ * @since 17/08/2023, 14:58:20
  *
  */
 window.blockly.js.blockly.Votacao.Voto.IniciarVotacaoArgs = [{ description: 'checkAceitarRegras', id: 'b8644fae' }];
 window.blockly.js.blockly.Votacao.Voto.IniciarVotacao = async function(checkAceitarRegras) {
- var idIdeiaVotada, checado;
+ var x, checkUnicoVoto;
   //
   checkAceitarRegras = this.cronapi.conversion.toBoolean(checkAceitarRegras);
   //
@@ -35,32 +35,38 @@ window.blockly.js.blockly.Votacao.Voto.IniciarVotacao = async function(checkAcei
 /**
  * @function ObtemVoto
  *
- *
+ * Trata a votação em si, verificando os votos selecionados no
+ * acordeão e alterando a tela de votos efetuados de acordo.
  *
  * @param idIdeiaVotada
  * @param checado
  *
  * @author José Zay
- * @since 17/08/2023, 14:50:55
+ * @since 17/08/2023, 14:58:20
  *
  */
 window.blockly.js.blockly.Votacao.Voto.ObtemVotoArgs = [{ description: 'idIdeiaVotada', id: '66436263' }, { description: 'checado', id: '7b36cee4' }];
 window.blockly.js.blockly.Votacao.Voto.ObtemVoto = async function(idIdeiaVotada, checado) {
- var lista;
+ var x;
   //
   lista = this.cronapi.screen.getValueOfField("vars.listaVotos");
   //
   if (this.cronapi.conversion.toBoolean(checado)) {
     //
     if (this.cronapi.logic.isNullOrEmpty(lista)) {
-      //
+      // Inicializa pela primeira vez a variável lista.
       lista = [];
     }
     //
     lista.push(idIdeiaVotada);
-    //
+    // Atualiza variável de tela.
     this.cronapi.screen.changeValueOfField("vars.listaVotos", lista);
-    //
+    // Controla visualizações da tela de votações efetuadas
+    // (da direita) conforme número de votos selecionados.
+    // -Com nenhum voto, deve aparecer uma tela 'vazia' esperando algo.
+    // -Com um voto, deve aparecer o voto selecionado e uma checkbox
+    // de confirmação se o votante irá apenas efetuar um voto.
+    // -Com dois votos deve aparecer apenas os votos e os botões para finalizar.
     if (lista.length <= 1) {
       //
       this.cronapi.screen.addClass("div-tela-espera-voto-id", 'hidden');
@@ -78,9 +84,14 @@ window.blockly.js.blockly.Votacao.Voto.ObtemVoto = async function(idIdeiaVotada,
   } else {
     //
     lista.splice(((lista.indexOf(idIdeiaVotada) + 1) - 1), 1);
-    //
+    // Atualiza variável de tela.
     this.cronapi.screen.changeValueOfField("vars.listaVotos", lista);
-    //
+    // Controla visualizações da tela de votações efetuadas
+    // (da direita) conforme número de votos selecionados.
+    // -Com nenhum voto, deve aparecer uma tela 'vazia' esperando algo.
+    // -Com um voto, deve aparecer o voto selecionado e uma checkbox
+    // de confirmação se o votante irá apenas efetuar um voto.
+    // -Com dois votos deve aparecer apenas os votos e os botões para finalizar.
     if (lista.length <= 0) {
       //
       this.cronapi.screen.removeClass("div-tela-espera-voto-id", 'hidden');
@@ -98,17 +109,18 @@ window.blockly.js.blockly.Votacao.Voto.ObtemVoto = async function(idIdeiaVotada,
 /**
  * @function FinalizarVotacao
  *
- *
+ * Valida se os votos informados são válidos em quantidade
+ * e então repassa os dados para serem gravados.
  *
  * @param checkUnicoVoto
  *
  * @author José Zay
- * @since 17/08/2023, 14:50:55
+ * @since 17/08/2023, 14:58:20
  *
  */
 window.blockly.js.blockly.Votacao.Voto.FinalizarVotacaoArgs = [{ description: 'checkUnicoVoto', id: '8e5f7fd0' }];
 window.blockly.js.blockly.Votacao.Voto.FinalizarVotacao = async function(checkUnicoVoto) {
- var idIdeiaVotada, checado;
+ var x, lista;
   //
   lista = this.cronapi.screen.getValueOfField("vars.listaVotos");
   //
