f37085a3922466cc6627dcc5b65aa3bf05be6251
José Zay <jose.zay@techne.com.br>
2023-08-18 16:21:34 +0000

    remover bloco de obter voto do Java.

--- src/main/java/blockly/Votacao/Votos.java
+++ src/main/java/blockly/Votacao/Votos.java
@@ -16,8 +16,8 @@ public static final int TIMEOUT = 300;
  *
  * @param listaIdItemVoto
  *
- * @author Wesley Miranda De Oliveira
- * @since 18/08/2023, 11:37:40
+ * @author José Zay
+ * @since 18/08/2023, 13:12:08
  *
  */
 public static Var FinalizarVoto(@ParamMetaData(description = "listaIdItemVoto", id = "fb0d967d") Var listaIdItemVoto) throws Exception {
@@ -50,64 +50,6 @@ public static Var FinalizarVoto(@ParamMetaData(description = "listaIdItemVoto", 
         Var.VAR_TRUE),Var.valueOf("id",userId));
     } else {
       {}
-    }
-    return Var.VAR_NULL;
-   }
- }.call();
-}
-
-/**
- *
- * @param idProjetoVotado
- * @param checado
- *
- * @author Wesley Miranda De Oliveira
- * @since 18/08/2023, 11:37:40
- *
- */
-public static Var ObtemVoto(@ParamMetaData(description = "idProjetoVotado", id = "2a202a21") Var idProjetoVotado, @ParamMetaData(description = "checado", id = "68bd3e97") Var checado) throws Exception {
- return new Callable<Var>() {
-
-   private Var lista = Var.VAR_NULL;
-
-   public Var call() throws Exception {
-    cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
-    Var.valueOf("vars.listaVotos"),
-    Var.valueOf("aaa"));
-    lista =
-    cronapi.screen.Operations.getValueOfField(
-    Var.valueOf("vars.listaVotos"));
-    System.out.println(lista.getObjectAsString());
-    System.out.println(
-    cronapi.screen.Operations.getValueOfField(
-    Var.valueOf("vars.listaVotos")).getObjectAsString());
-    if (
-    Var.valueOf(
-    cronapi.logic.Operations.isNullOrEmpty(lista).getObjectAsBoolean() && checado.getObjectAsBoolean()).getObjectAsBoolean()) {
-        cronapi.list.Operations.setFirst(lista,idProjetoVotado);
-        cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
-        Var.valueOf("vars.listaVotos"), lista);
-        System.out.println(
-        Var.valueOf("1").getObjectAsString());
-    } else if (
-    Var.valueOf(checado.getObjectAsBoolean() &&
-    Var.valueOf(
-    cronapi.list.Operations.size(lista).compareTo(
-    Var.valueOf(2)) < 0).getObjectAsBoolean()).getObjectAsBoolean()) {
-        cronapi.list.Operations.setLast(lista,idProjetoVotado);
-        cronapi.util.Operations.callClientFunction(Var.valueOf("cronapi.screen.changeValueOfField"),
-        Var.valueOf("vars.listaVotos"), lista);
-        System.out.println(
-        Var.valueOf("2").getObjectAsString());
-    } else if (checado
-    .negate().getObjectAsBoolean()) {
-        cronapi.list.Operations.remove(lista,
-        cronapi.list.Operations.findFirst(lista, idProjetoVotado));
-        System.out.println(
-        Var.valueOf("3").getObjectAsString());
-    } else {
-        System.out.println(
-        Var.valueOf("Senão").getObjectAsString());
     }
     return Var.VAR_NULL;
    }
