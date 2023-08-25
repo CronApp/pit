d3aa632a18c4a1570cac1fd7bb3b3149382c697c
Silvio De Oliveira Carlos <silvio.carlos@techne.com.br>
2023-08-25 15:00:30 +0000

    Defeito #2275: Corrigir as permissionaveis do perfil Admin

--- src/main/java/blockly/Usuario/UsuarioComite.java
+++ src/main/java/blockly/Usuario/UsuarioComite.java
@@ -13,8 +13,8 @@ public static final int TIMEOUT = 300;
 
 /**
  *
- * @author José Zay
- * @since 24/08/2023, 10:48:35
+ * @author Silvio De Oliveira Carlos
+ * @since 25/08/2023, 11:59:41
  *
  */
 public static Var RetornaUsarioPertenceComite() throws Exception {
@@ -24,7 +24,7 @@ public static Var RetornaUsarioPertenceComite() throws Exception {
 
    public Var call() throws Exception {
     usuarioPertenceComite =
-    cronapi.database.Operations.query(Var.valueOf("app"),Var.valueOf("SELECT \n	SEC.NAME, COUNT(SEC.NAME)\nFROM \n	SECURABLE sec \nLEFT JOIN USER_SECURABLE usec ON (sec.id = usec.securable_id) \nLEFT JOIN USER us ON (us.id = usec.user_id)\nWHERE normalized_email = :userName\nAND (SEC.NAME = \'Administrators\' OR SEC.NAME = \'Comite\')\nGROUP BY SEC.NAME"),Var.valueOf("userName",
+    cronapi.database.Operations.query(Var.valueOf("app"),Var.valueOf("SELECT \n	SEC.NAME, COUNT(SEC.NAME)\nFROM \n	SECURABLE sec \nLEFT JOIN USER_SECURABLE usec ON (sec.id = usec.securable_id) \nLEFT JOIN USER us ON (us.id = usec.user_id)\nWHERE (normalized_email = :userName OR normalized_user_name = :userName)\nAND (SEC.NAME = \'Administrators\' OR SEC.NAME = \'Comite\')\nGROUP BY SEC.NAME"),Var.valueOf("userName",
     cronapi.util.Operations.getCurrentUserName()));
     return
 cronapi.logic.Operations.isNullOrEmpty(usuarioPertenceComite)
