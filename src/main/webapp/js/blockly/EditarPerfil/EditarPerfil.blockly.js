921b5583795596e0c931a7dcacd5e2fcd52b6cda
José Zay <jose.zay@techne.com.br>
2023-08-28 20:06:51 +0000

    fazer dar foco nos componentes.

--- src/main/webapp/js/blockly/EditarPerfil/EditarPerfil.blockly.js
+++ src/main/webapp/js/blockly/EditarPerfil/EditarPerfil.blockly.js
@@ -11,12 +11,12 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil = window.blockly.js.blockly.
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.NotificacaoLoginArgs = [];
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.NotificacaoLogin = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   this.cronapi.notification.customNotify('success', ['Seja bem-vindo(a), ',this.cronapi.util.getUserName(),', ao Sistema de Votação PIT (Prêmio de Inovação Techne).'].join(''), 'fade', 'bottom', 'right', 'true');
 }
@@ -28,12 +28,12 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.NotificacaoLogin = async fun
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.PreencheCampoNomeArgs = [];
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.PreencheCampoNome = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   this.cronapi.screen.changeValueOfField("vars.nomeUsuarioEdicao", this.cronapi.util.getUserName());
   //
@@ -47,12 +47,12 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.PreencheCampoNome = async fu
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.FecharModalEditarPerfilArgs = [];
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.FecharModalEditarPerfil = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   this.cronapi.screen.hideModal("modalEdicaoPerfil");
 }
@@ -64,12 +64,12 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.FecharModalEditarPerfil = as
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditarArgs = [];
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditar = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   this.cronapi.screen.showModal("modalEdicaoPerfil");
 }
@@ -81,12 +81,12 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditar = async f
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditarSenhaArgs = [];
 window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditarSenha = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   this.cronapi.screen.showModal("modalPassword");
   //
@@ -94,7 +94,7 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditarSenha = as
 }
 
 /**
- * @function validarAlterarSenha
+ * @function ValidarAlterarSenha
  *
  * normalmente, no elemento que chama esta função, deveria ter o
  * seguinte atributo: 'ng-click="changePassword()"' para efetuar
@@ -105,26 +105,39 @@ window.blockly.js.blockly.EditarPerfil.EditarPerfil.IniciarModalEditarSenha = as
  *
  *
  * @author José Zay
- * @since 28/08/2023, 14:31:05
+ * @since 28/08/2023, 16:48:48
  *
  */
-window.blockly.js.blockly.EditarPerfil.EditarPerfil.validarAlterarSenhaArgs = [];
-window.blockly.js.blockly.EditarPerfil.EditarPerfil.validarAlterarSenha = async function() {
- var reset, retorno, newPasswordConfirmation, oldPassword, newPassword;
+window.blockly.js.blockly.EditarPerfil.EditarPerfil.ValidarAlterarSenhaArgs = [];
+window.blockly.js.blockly.EditarPerfil.EditarPerfil.ValidarAlterarSenha = async function() {
+ var oldPassword, newPassword, newPasswordConfirmation;
   //
   oldPassword = this.cronapi.util.executeJavascriptReturn('document.getElementById(\"oldPassword\").value');
   //
   newPassword = this.cronapi.util.executeJavascriptReturn('document.getElementById(\"newPassword\").value');
+  //
+  newPasswordConfirmation = this.cronapi.util.executeJavascriptReturn('document.getElementById(\"newPasswordConfirmation\").value');
   // Aqui são feitas as validações que faltam dentro da chamada changePassword.
   if (this.cronapi.logic.isNullOrEmpty(oldPassword)) {
     //
     this.cronapi.screen.notify('error','Senha Anterior não pode estar vazio.');
+    //
+    this.cronapi.screen.focusComponent("oldPassword");
   } else if (this.cronapi.logic.isNullOrEmpty(newPassword)) {
     //
     this.cronapi.screen.notify('error','Nova Senha não pode estar vazio.');
+    //
+    this.cronapi.screen.focusComponent("newPassword");
   } else if (newPassword.length < 8) {
     //
     this.cronapi.screen.notify('error','O tamanho da senha deve ser de no mínimo 8 caracteres.');
+    //
+    this.cronapi.screen.focusComponent("newPassword");
+  } else if (newPassword != newPasswordConfirmation) {
+    //
+    this.cronapi.screen.notify('error','Os campos Nova Senha e Confirmação não coincidem.');
+    //
+    this.cronapi.screen.focusComponent("newPasswordConfirmation");
   } else {
     //
     this.cronapi.util.executeJavascriptNoReturn('angular.element(document.getElementById(\'btn-override-salvar-senha\')).scope().changePassword()');
