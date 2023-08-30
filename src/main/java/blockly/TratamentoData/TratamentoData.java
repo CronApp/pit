fdd49b081ece2fee93f1acd05ba277bbc6125310
José Zay <jose.zay@techne.com.br>
2023-08-30 12:42:51 +0000

    ajustar data para 25/11;

--- src/main/java/blockly/TratamentoData/TratamentoData.java
+++ src/main/java/blockly/TratamentoData/TratamentoData.java
@@ -16,7 +16,7 @@ public static final int TIMEOUT = 300;
 /**
  *
  * @author José Zay
- * @since 24/08/2023, 15:38:14
+ * @since 30/08/2023, 09:06:30
  *
  */
 @RequestMapping(path = "/api/cronapi/rest/TratamentoData.TratamentoData:ResultadoData", method = RequestMethod.GET, consumes = "*/*")
@@ -29,7 +29,7 @@ Var.valueOf(
 cronapi.dateTime.Operations.getNowNoHour().compareTo(
 cronapi.dateTime.Operations.newDate(
 Var.valueOf(2023),
-Var.valueOf(8),
+Var.valueOf(11),
 Var.valueOf(25),
 Var.VAR_NULL,
 Var.VAR_NULL,
