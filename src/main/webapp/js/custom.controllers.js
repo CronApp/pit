bff29248a698b384cc69d679e095107315c1c146
José Zay <jose.zay@techne.com.br>
2023-08-30 16:30:55 +0000

    verificar requisitos de senha utilizando angular.

--- src/main/webapp/js/custom.controllers.js
+++ src/main/webapp/js/custom.controllers.js
@@ -3,7 +3,28 @@ app.controller('AfterLoginController', function($scope) {
 });
 
 app.controller('AfterHomeController', function($scope) {
- //
+    $scope.passwordRequirementsVisible = false;
+    $scope.isLengthValid = false;
+    $scope.isAlphaNumericValid = false;
+    $scope.isUpperCaseValid = false;
+    $scope.isSpecialCharValid = false;
+
+    $scope.checkPasswordRequirements = function() {
+        const password = $scope.password;
+
+        $scope.isLengthValid = password.length > 8;
+        $scope.isAlphaNumericValid = /[a-zA-Z]/.test(password) && /\d/.test(password);
+        $scope.isUpperCaseValid = /[A-Z]/.test(password);
+        $scope.isSpecialCharValid = /[\W_]/.test(password);
+    };
+
+    $scope.showPasswordRequirements = function() {
+        $scope.passwordRequirementsVisible = true;
+    };
+
+    $scope.hidePasswordRequirements = function() {
+        $scope.passwordRequirementsVisible = false;
+    };
 });
 
 app.controller('AfterPageController', function($scope) {
