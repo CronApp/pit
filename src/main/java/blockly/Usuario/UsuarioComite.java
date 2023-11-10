3f8e513462c96060892f7c4944152b3f703724d3
José Zay <jose.zay@techne.com.br>
2023-11-10 19:15:30 +0000

    correção do bug de publicação em bancos não h2.

--- src/main/java/blockly/Usuario/UsuarioComite.java
+++ src/main/java/blockly/Usuario/UsuarioComite.java
@@ -13,8 +13,8 @@ public static final int TIMEOUT = 300;
 
 /**
  *
- * @author Silvio De Oliveira Carlos
- * @since 25/08/2023, 17:41:56
+ * @author José Zay
+ * @since 10/11/2023, 16:11:06
  *
  */
 public static Var RetornaUsarioPertenceComite() throws Exception {
@@ -24,7 +24,7 @@ public static Var RetornaUsarioPertenceComite() throws Exception {
 
    public Var call() throws Exception {
     usuarioPertenceComite =
-    cronapi.database.Operations.query(Var.valueOf("app"),Var.valueOf("SELECT us.NAME,\n       COUNT(us.NAME)\nFROM USER us\nLEFT JOIN USER_SECURABLE usec ON (us.id = usec.user_id)\nLEFT JOIN SECURABLE sec ON (sec.id = usec.securable_id)\nLEFT JOIN USER_ROLE ur ON (ur.user_id = us.id)\nWHERE ((normalized_email = :userName\n        OR normalized_user_name = :userName)\n       AND ((SEC.NAME = \'Administrators\'\n             OR SEC.NAME = \'Comite\'))\n       OR ((normalized_email = :userName\n            OR normalized_user_name = :userName)\n           AND ROLE_ID =\n             (SELECT ID\n              FROM ROLE\n              WHERE NORMALIZED_NAME = \'administrators\')))\nGROUP BY us.NAME"),Var.valueOf("userName",
+    cronapi.database.Operations.query(Var.valueOf("app"),Var.valueOf("SELECT us.NAME,\n       COUNT(us.NAME)\nFROM USER us\nLEFT JOIN USER_SECURABLE usec ON (us.id = usec.user_id)\nLEFT JOIN SECURABLE sec ON (sec.id = usec.securable_id)\nLEFT JOIN USER_ROLE ur ON (ur.user_id = us.id)\nWHERE ((normalized_email = :userName\n        OR normalized_user_name = :userName)\n       AND ((sec.name = \'Administrators\'\n             OR sec.name = \'Comite\'))\n       OR ((normalized_email = :userName\n            OR normalized_user_name = :userName)\n           AND ROLE_ID =\n             (SELECT ID\n              FROM ROLE\n              WHERE NORMALIZED_NAME = \'administrators\')))\nGROUP BY us.NAME"),Var.valueOf("userName",
     cronapi.util.Operations.getCurrentUserName()));
     return
 cronapi.logic.Operations.isNullOrEmpty(usuarioPertenceComite)
